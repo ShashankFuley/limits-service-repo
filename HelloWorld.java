7dc2fe591e5821f951061c64b9e27ea01a319837
ShashankFuley <shashankfuley@gmail.com>
2020-07-30 12:01:11 +0530

    This commit is for jenkins Poll scm

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 class HelloWorld{
 	public static void main(String shashank[]){
-		System.out.println("Hello World!! This is git hub");
+		System.out.println("Hello World!! This is Poll SCM edition");
 	}
 }
